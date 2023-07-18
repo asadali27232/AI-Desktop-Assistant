011a765ef214393d54e0bd32c9fee156f01d0cec
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-18 20:32:01 +0500

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import speech_recognition as sr
 import win32com.client as wincl
+import os
+import webbrowser
 
 
 def say(text):
@@ -15,4 +18,40 @@ def say(text):
     speak.Speak(text)
 
 
-say("Hello, I am your personal assistant. What can I do for you?")
+def listen():
+    # Object to recognize text from audio
+    r = sr.Recognizer()
+
+    # Using the microphone as source for input
+    with sr.Microphone() as source:
+        print("Listening...")
+        # Pause for 1 second and create the audio source file
+        r.pause_threshold = 0.5
+        # Listening and storing speech in audio
+        audio = r.listen(source)
+        try:
+            # Recognizing the text from audio
+            print("Recognizing...")
+            text = r.recognize_google(audio, language="en-in")
+            print("You said: {}".format(text))
+            return text
+        except:
+            print("Sorry, I didn't get that.")
+            return "Sorry, I didn't get that."
+
+
+def execute_system_command(command):
+    os.system(command)
+
+
+query = listen()
+query = query.lower()
+if "open" in query:
+    if "calculator" in query:
+        os.system("calc.exe")
+    elif "notepad" in query:
+        os.system("notepad.exe")
+    elif "vs code" in query:
+        os.system("code")
+    elif "youtube" in query:
+        webbrowser.open("https://youtube.com")
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-import openai
+# import openai
 
-openai.api_key = "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"
+# openai.api_key = "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"
 
-request = {"role": "user", "content": "Hello, how are you?"}
+# request = {"role": "user", "content": "Hello, how are you?"}
 
-response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[request], temperature=1, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0)
+# response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[request], temperature=1, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0)
 
-print(response.choices[0]["message"]["content"])
+# print(response.choices[0]["message"]["content"])
+
+import os
+os.system('calc.exe')
