be135560b4232b8f14604236eba9b9dc448899c6
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-18 21:38:46 +0500

    Updated

---
+++ WebQueries.py
@@ -0,0 +1,28 @@
+import webbrowser
+
+
+class WebQueries:
+    def __init__(self) -> None:
+        pass
+
+    def open_site(self, query):
+        if "open youtube" in query:
+            webbrowser.open("youtube.com")
+        elif "open google" in query:
+            webbrowser.open("google.com")
+        elif "open stackoverflow" in query:
+            webbrowser.open("stackoverflow.com")
+        elif "open github" in query:
+            webbrowser.open("github.com")
+        elif "open facebook" in query:
+            webbrowser.open("facebook.com")
+        elif "open instagram" in query:
+            webbrowser.open("instagram.com")
+        elif "open twitter" in query:
+            webbrowser.open("twitter.com")
+        elif "open linkedin" in query:
+            webbrowser.open("linkedin.com")
+        elif "open reddit" in query:
+            webbrowser.open("reddit.com")
+        elif "open wikipedia":
+            webbrowser.open("wikipedia.com")
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from Sayer import Sayer
 from Listener import Listener
 from SystemQueries import SystemQueries
-import webbrowser
+from WebQueries import WebQueries
 
 sayer = Sayer()
 listener = Listener()
 system_queries = SystemQueries()
-
+web_queries = WebQueries()
 sayer.say("Hello, I am your personal assistant. How may I help you?")
 
 query = listener.listen()
 
 query = query.lower()
-if "open youtube" in query:
-    webbrowser.open("youtube.com")
 
-
-system_queries.process_query(query)
+web_queries.open_site(query)
