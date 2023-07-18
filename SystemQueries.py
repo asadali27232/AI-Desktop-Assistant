df125c64f34cb21f6c246e2f91924627217bcdff
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-18 21:19:05 +0500

    Updated

--- SystemQueries.py
+++ SystemQueries.py
@@ -1,5 +1,24 @@
-class SystemQueries:
-    def __init__(self):
-        pass
+import os
 
-    
+
+class SystemQueries:
+    def execute_system_command(self, command):
+        os.system(command)
+
+    def process_query(self, query):
+        if "calculator" in query:
+            self.execute_system_command("calc.exe")
+        elif "notepad" in query:
+            self.execute_system_command("notepad.exe")
+        elif "file explorer" in query:
+            self.execute_system_command("explorer")
+        elif "task manager" in query:
+            self.execute_system_command("taskmgr")
+        elif "paint" in query:
+            self.execute_system_command("mspaint")
+        elif "command prompt" in query:
+            self.execute_system_command("cmd.exe")
+        elif "control panel" in query:
+            self.execute_system_command("control")
+        else:
+            print(query)
--- test.py
+++ test.py
@@ -8,60 +8,6 @@
 
 # print(response.choices[0]["message"]["content"])
 
-# import os
-
-# os.system("calc.exe")
-
-import speech_recognition as sr
-import win32com.client as wincl
 import os
-import webbrowser
 
-
-def say(text):
-    # Setting the voice engine for the assistant to speak
-    # Using the built in SAPI's SAPI.SpVoice class
-    speak = wincl.Dispatch("SAPI.SpVoice")
-
-    # Setting the voice type for the assistant, 0 for male voice, 1 for female voice
-    speak.Voice = speak.GetVoices().Item(1)
-
-    # Set the speech rate (speed)
-    speak.Rate = 2
-
-    speak.Speak(text)
-
-
-say("Hello, I am your personal assistant. What can I do for you?")
-
-
-def listen():
-    # Object to recognize text from audio
-    r = sr.Recognizer()
-
-    # Using the microphone as source for input
-    with sr.Microphone() as source:
-        print("Listening...")
-        # Pause for 1 second and create the audio source file
-        r.pause_threshold = 0.5
-        # Listening and storing speech in audio
-        audio = r.listen(source)
-        try:
-            # Recognizing the text from audio
-            print("Recognizing...")
-            text = r.recognize_google(audio, language="en-in")
-            print("You said: {}".format(text))
-            return text
-        except:
-            print("Sorry, I didn't get that.")
-            return "Sorry, I didn't get that."
-
-
-def execute_system_command(command):
-    os.system(command)
-
-
-query = listen()
-query = query.lower()
-if "open youtube" in query:
-    webbrowser.open("https://youtube.com")
+os.system("calc.exe")
